da3578b31593a3c47cf9bcdcf65e39ed4946fd25
Bunyamin YASAR <44155321+yasarbunyamin@users.noreply.github.com>
2020-11-25 00:00:10 +0300

    Update Main.java

--- javaMinTrianglePath/src/com/company/Main.java
+++ javaMinTrianglePath/src/com/company/Main.java
@@ -48,12 +48,11 @@ class Main {
     public static void MinTrianglePath(List<List<Integer>> triangle) {
 
 
-        int[] dp = new int[triangle.size()+1];
-        int[] dm = new int[triangle.size()+1];
+        int[] lastArray = new int[triangle.size()+1];
 
         // inital the dp[] array
         for(int i=0; i<triangle.size(); i++) {
-            dp[i] += triangle.get(triangle.size()-1).get(i);
+            lastArray[i] += triangle.get(triangle.size()-1).get(i);
         }
 
         // from bottom to up
@@ -62,16 +61,16 @@ class Main {
             int location = 0;
             // each num in current list
             for(int j=0; j<i+1; j++) {
-               min = Math.min(dp[j], dp[j + 1]);
+               min = Math.min(lastArray[j], lastArray[j + 1]);
                 location =triangle.get(i).get(j);
-                dp[j] =location  + min;
+                lastArray[j] =location  + min;
 
 
             }
 
 
         }
-        System.out.println("Minimum Triangle Path: " + dp[0]);
+        System.out.println("Minimum Triangle Path: " + lastArray[0]);
 
     }
 
